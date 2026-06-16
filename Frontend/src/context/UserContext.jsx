0490d40b9b7e4b0fcc73f8349e9a0641d0c05d67
Jinay Shah <jinay@Jinays-MacBook-Pro.local>
2026-06-16 16:01:00 +0530

    Cloudinary setup

--- Frontend/src/context/UserContext.jsx
+++ Frontend/src/context/UserContext.jsx
@@ -1,38 +1,39 @@
-import React, { createContext, useEffect, useState} from 'react'
+import React, { createContext, useEffect, useState } from 'react'
 import axios from "axios"
 
 export const userDataContext = createContext()
 
-const serverUrl = "http://localhost:8000"
+const UserContext = ({ children }) => {
 
-const [userData,setUserData] = useState(null);
+  const serverUrl = "http://localhost:8000"
 
-const value = {
+  const value = {
     serverUrl
-}
-
-const handleCurrentUser = async ()=>{
-  try{
-    const result = await axios.get(`${serverUrl}/api/auth/user/current`,{withCredentials:true})
-    setUserData(result.data)
-    console.log(result.data)
   }
-  catch(error){
-    console.log(error)
+
+  const handleCurrentUser = async () => {
+    try {
+      const result = await axios.get(`${serverUrl}/api/auth/user/current`, { withCredentials: true })
+      setUserData(result.data)
+      console.log(result.data)
+    }
+    catch (error) {
+      console.log(error)
+    }
   }
-}
 
-useEffect(()=>{
-  handleCurrentUser()
-},[])
+  useEffect(() => {
+    handleCurrentUser()
+  }, [])
 
-const UserContext = ({children}) => {
+  const [userData, setUserData] = useState(null);
+
   return (
     <div>
-        <userDataContext.Provider value={value}>
-            {children}
-        </userDataContext.Provider>
-        
+      <userDataContext.Provider value={value}>
+        {children}
+      </userDataContext.Provider>
+
     </div>
   )
 }
